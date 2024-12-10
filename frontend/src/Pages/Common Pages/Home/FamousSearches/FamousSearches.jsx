f072fd29b6f23748a5b19679fbab23be2f9d05db
nikosmxl <nikosmixaloutsos@gmail.com>
2024-12-10 18:46:05 +0200

    organized previous commit

---
+++ frontend/src/Pages/Common Pages/Home/FamousSearches/FamousSearches.jsx
@@ -0,0 +1,81 @@
+import s from "./FamousSearchesStyle.module.css"
+import { useState } from "react";
+
+function FamousSearches(){
+    const [checkedItems, setCheckedItems] = useState({
+        cooking: false,
+        firstAid: false,
+        babysitterCertificate: false,
+        homeworkHelp: false,
+        englishNativeSpeaker: false
+    });
+
+    const handleCheckboxChange = (event) => {
+        const { name, checked } = event.target;
+        setCheckedItems((prevState) => ({
+            ...prevState,
+            [name]: checked,
+        }));
+    };
+
+    return (
+        <div className={s.famous_searches}>
+            <p>Δημοφιλείς αναζητήσεις</p>
+            <label className={s.checkbox_label}>
+                <input
+                type="checkbox"
+                name="cooking"
+                checked={checkedItems.cooking}
+                onChange={handleCheckboxChange}
+                />
+                <span className={s.checkmark}></span>
+                Μαγείρεμα
+            </label>
+            <label className={s.checkbox_label}>
+                <input
+                type="checkbox"
+                name="firstAid"
+                checked={checkedItems.firstAid}
+                onChange={handleCheckboxChange}
+                />
+                <span className={s.checkmark}></span>
+                Α` βοήθειες
+            </label>
+            <label className={s.checkbox_label}>
+                <input
+                type="checkbox"
+                name="babysitterCertificate"
+                checked={checkedItems.babysitterCertificate}
+                onChange={handleCheckboxChange}
+                />
+                <span className={s.checkmark}></span>
+                Πιστοποίηση Νταντά
+            </label>
+            <label className={s.checkbox_label}>
+                <input
+                type="checkbox"
+                name="homeworkHelp"
+                checked={checkedItems.homeworkHelp}
+                onChange={handleCheckboxChange}
+                />
+                <span className={s.checkmark}></span>
+                Βοήθεια με μαθήματα
+            </label>
+            <label className={s.checkbox_label}>
+                <input
+                type="checkbox"
+                name="englishNativeSpeaker"
+                checked={checkedItems.englishNativeSpeaker}
+                onChange={handleCheckboxChange}
+                />
+                <span className={s.checkmark}></span>
+                English Native Speaker
+            </label>
+            <div className={s.famous_searches_search_button}>
+                <p>Αναζήτηση</p>
+            </div>
+        </div>
+    )
+}
+
+export default FamousSearches;
--- frontend/src/Pages/Common Pages/Home/Home.jsx
+++ frontend/src/Pages/Common Pages/Home/Home.jsx
@@ -1,25 +1,9 @@
 import s from "./HomeStyle.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import FamousSearches from "./FamousSearches/FamousSearches";
 
 function Home(){
-    const [checkedItems, setCheckedItems] = useState({
-        cooking: false,
-        firstAid: false,
-        babysitterCertificate: false,
-        homeworkHelp: false,
-        englishNativeSpeaker: false
-    });
-
-    const handleCheckboxChange = (event) => {
-        const { name, checked } = event.target;
-        setCheckedItems((prevState) => ({
-            ...prevState,
-            [name]: checked,
-        }));
-    };
-
     return (
         <div className={s.home_page}>
             <div className={s.background_pic}>
@@ -35,62 +19,7 @@ function Home(){
                                 </div>
                             </div>
                         </div>
-                        <div className={s.famous_searches}>
-                            <p>Δημοφιλείς αναζητήσεις</p>
-                            <label className={s.checkbox_label}>
-                                <input
-                                type="checkbox"
-                                name="cooking"
-                                checked={checkedItems.cooking}
-                                onChange={handleCheckboxChange}
-                                />
-                                <span className={s.checkmark}></span>
-                                Μαγείρεμα
-                            </label>
-                            <label className={s.checkbox_label}>
-                                <input
-                                type="checkbox"
-                                name="firstAid"
-                                checked={checkedItems.firstAid}
-                                onChange={handleCheckboxChange}
-                                />
-                                <span className={s.checkmark}></span>
-                                Α` βοήθειες
-                            </label>
-                            <label className={s.checkbox_label}>
-                                <input
-                                type="checkbox"
-                                name="babysitterCertificate"
-                                checked={checkedItems.babysitterCertificate}
-                                onChange={handleCheckboxChange}
-                                />
-                                <span className={s.checkmark}></span>
-                                Πιστοποίηση Νταντά
-                            </label>
-                            <label className={s.checkbox_label}>
-                                <input
-                                type="checkbox"
-                                name="homeworkHelp"
-                                checked={checkedItems.homeworkHelp}
-                                onChange={handleCheckboxChange}
-                                />
-                                <span className={s.checkmark}></span>
-                                Βοήθεια με μαθήματα
-                            </label>
-                            <label className={s.checkbox_label}>
-                                <input
-                                type="checkbox"
-                                name="englishNativeSpeaker"
-                                checked={checkedItems.englishNativeSpeaker}
-                                onChange={handleCheckboxChange}
-                                />
-                                <span className={s.checkmark}></span>
-                                English Native Speaker
-                            </label>
-                            <div className={s.famous_searches_search_button}>
-                                <p>Αναζήτηση</p>
-                            </div>
-                        </div>
+                        <FamousSearches />
                     </div>
                     <h1>Βρείτε νταντά ή εργαστείτε ως νταντά,
                         <span> εύκολα </span>
