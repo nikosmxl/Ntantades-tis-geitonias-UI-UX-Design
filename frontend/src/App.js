8f0f20a7d8c5ba0ca287343ccf7d76cc62a2bc6a
ChristosKypraios <christoskupraios@gmail.com>
2024-12-14 20:10:53 +0200

    patched routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoggedOutLayout from "./Layouts/LoggedOutLayout";
-import ParentLayout from './Layouts/ParentLayout';
-import BabysitterLayout from './Layouts/BabysitterLayout';
-import LiveChat from './components/Live Chat/LiveChat';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import LiveChat from './Components/Live Chat/LiveChat';
+import WelcomeNavBar from './Components/WelcomeNavBar/WelcomeNavBar';
+import NavBar from './Components/NavBar/NavBar';
+import Home from './Pages/Common Pages/Home/Home';
+import routes from './routes';
 
 function App() {
+
+  const getRoutes = (context) => {
+    return routes.filter((route) => route.context === context).map((route) => {
+      return <Route
+        key={`${context}/${route.path}/route`}    // monadiko key
+        path={route.path}
+        element={route.page}
+      />
+    });
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,21 +25,43 @@ function App() {
             <Route
               path='/'
               element={
-                <LoggedOutLayout/>
+                <>
+                  <WelcomeNavBar />
+                  <Outlet />
+                </>
               }
-            />
+            >
+              <Route index element={<Home />} /> {/* Default sub-route */}
+              { getRoutes('loggedOut') }
+              <Route path='*' element={<Navigate to='/' replace />} /> {/* Return to home page if invalid route */}
+            </Route>
             <Route
               path='/parent'
               element={
-                <ParentLayout/>
+                <>
+                  <NavBar />
+                  <Outlet />
+                </>
               }
-            />
+              exact
+            >
+              <Route index element={<Home />} /> {/* Default sub-route */}
+              { getRoutes('parent') }
+              <Route path='*' element={<Navigate to='/' replace />} /> {/* Return to home page if invalid route */}
+            </Route>
             <Route
               path='/babysitter'
               element={
-                <BabysitterLayout/>
+                <>
+                  <NavBar />
+                  <Outlet />
+                </>
               }
-            />
+            >
+              <Route index element={<Home />} /> {/* Default sub-route */}
+              { getRoutes('babysitter') }
+              <Route path='*' element={<Navigate to='/' replace />} /> {/* Return to home page if invalid route */}
+            </Route>
           </Routes>
         </div>
         <LiveChat />
