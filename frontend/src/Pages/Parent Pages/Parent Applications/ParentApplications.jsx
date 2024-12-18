930a699c0af83f682327739cd797abc2eeec4769
nikosmxl <nikosmixaloutsos@gmail.com>
2024-12-18 02:25:10 +0200

    application done

---
+++ frontend/src/Pages/Parent Pages/Parent Applications/ParentApplications.jsx
@@ -0,0 +1,27 @@
+import s from "./ParentApplicationsStyle.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Application from "../../../Components/Application/Application";
+import { faClockRotateLeft } from "@fortawesome/free-solid-svg-icons";
+
+function ParentApplications(){
+    
+    return (
+        <div className={s.applications_page}>
+            <div className={s.breadcrumbs}>
+                <p>Αρχική</p>
+                <p>{">"}</p>
+                <p>Αιτήσεις</p>
+            </div>
+            <div className={s.title_history_row}>
+                <h3>Οι αιτήσεις μου</h3>
+                <button className={s.history_button}>
+                    <FontAwesomeIcon icon={faClockRotateLeft} />
+                    Ιστορικό αιτήσεων
+                </button>
+            </div>
+            <Application isParent={true} application_state={0} isHistory={false} isEditable={true} />
+        </div>
+    )
+}
+
+export default ParentApplications;
