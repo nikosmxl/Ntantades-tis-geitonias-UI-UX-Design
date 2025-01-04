ee81c48ec4eca6afda211a854774c838c8809374
ChristosKypraios <christoskupraios@gmail.com>
2025-01-04 02:07:05 +0200

    created Payment component

---
+++ frontend/src/Components/Payment/Payment.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import s from './PaymentStyle.module.css';
+import troll_prof from "../../Assets/Pictures/troll_prof.jpg";
+import qr_code from "../../Assets/Pictures/qr_code.png";
+import ExpandButtons from '../ExpandButtons/ExpandButtons';
+
+const Payment = ({ payment, isParent = true }) => {
+  const [isExpanded, setExpanded] = useState(false);
+  
+  const toggleIsExpanded = () => {
+    setExpanded(!isExpanded);
+  };
+
+  return (
+    <div className={s.payment_container}>
+
+      <div
+        className={`${s.payment} ${isExpanded ? s.open : ''}`}
+        style={{
+          borderColor: payment.status === 'redeemed' ? '#429602' : '#2761CE',
+        }}
+      >
+        <div className={s.payment_top_row}>
+          <div className={s.payment_general_info}>
+            <img src={troll_prof} alt="Profile" />
+            <b className={s.name}>Δήμητρα Χατζή</b>
+            <p>•</p>
+            {payment.status === 'redeemed'
+            ?
+              <p>Έγινε εξαργύρωση</p>
+            :
+              <p>Εκκρεμεί εξαργύρωση</p>
+            }
+          </div>
+          <div className={s.first_row_right_side}>
+            <p className={`${s.payment_date} ${!isExpanded ? s.not_expanded : ''}`}>22/12/2024</p>
+          </div>
+        </div>
+        <div className={s.payment_details}>
+          <div className={s.view_agreement}>
+            <h4>ΠΡΟΒΟΛΗ ΣΥΜΦΩΝΗΤΙΚΟΥ ΣΥΝΕΡΓΑΣΙΑΣ</h4>
+          </div>
+          <hr />
+          <div className={s.qr_container}>
+            <p>Κωδικός QR:</p>
+            <img src={qr_code} alt="Payment QR Code"/>
+          </div>
+        </div>
+      </div>
+
+      <ExpandButtons
+        isExpanded={isExpanded}
+        toggleIsExpanded={toggleIsExpanded}
+        showExpandButton={isParent}
+        showOptionsButtons={false}
+        showDeleteButton={false} 
+        showDeclineButton={false}
+        showEditButton={false}
+        showAcceptButton={!false} 
+      />
+    </div>
+  );
+}
+ 
+export default Payment;
