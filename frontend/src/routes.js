867e21d9731291bbbc1f24c89113665be25c3279
sdi1800011 <teoarg13@gmail.com>
2025-01-08 20:06:48 +0200

    Complete Signup, BabysitterDates, ParentDetails

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -9,6 +9,10 @@ import ParentPartnership from "./Pages/Parent Pages/Parent Partnership/ParentPar
 import ParentDates from "./Pages/Parent Pages/ParentDates/ParentDates";
 import BabysitterPartnership from "./Pages/Babysitter Pages/Babysitter Partnership/BabysitterPartnership";
 import EditDate from "./Pages/Common Pages/EditDate/EditDate";
+import Signup from "./Pages/Common Pages/Member Signup/Signup";
+import BabysitterDates from "./Pages/Babysitter Pages/Babysitter Dates/BabysitterDates";
+import ParentDetails from "./Pages/Babysitter Pages/Parent Details/ParentDetails";
+
 
 /*
   Εδώ ορίζουμε όλα τα routes.
@@ -77,6 +81,25 @@ const routes = [
     path: 'edit-date/:id',
     page: <EditDate />,
   },
+  {
+    context: 'loggedOut',
+    path: 'Signup',
+    page: <Signup />,
+  },
+
+  {
+    context: 'babysitter',
+    path: 'dates',
+    page: <BabysitterDates />,
+  },
+
+  {
+    context: 'babysitter',
+    path: 'parent-details',
+    page: <ParentDetails />,
+  },
+
+
 ];
 
 export default routes;
